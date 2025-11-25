431ab10787d78e149115248ba754fc412a59c11e
Abdelrahman Khattab <abzokhattab@gmail.com>
2025-11-25 09:21:59 +0100

    addign approve failed types

--- src/libs/ReportActionsUtils.ts
+++ src/libs/ReportActionsUtils.ts
@@ -246,6 +246,10 @@ function isDynamicExternalWorkflowSubmitFailedAction(reportAction: OnyxInputOrEn
     return isActionOfType(reportAction, CONST.REPORT.ACTIONS.TYPE.DEW_SUBMIT_FAILED);
 }
 
+function isDynamicExternalWorkflowApproveFailedAction(reportAction: OnyxInputOrEntry<ReportAction>): reportAction is ReportAction<typeof CONST.REPORT.ACTIONS.TYPE.DEW_APPROVE_FAILED> {
+    return isActionOfType(reportAction, CONST.REPORT.ACTIONS.TYPE.DEW_APPROVE_FAILED);
+}
+
 function isModifiedExpenseAction(reportAction: OnyxInputOrEntry<ReportAction>): reportAction is ReportAction<typeof CONST.REPORT.ACTIONS.TYPE.MODIFIED_EXPENSE> {
     return isActionOfType(reportAction, CONST.REPORT.ACTIONS.TYPE.MODIFIED_EXPENSE);
 }
@@ -3464,6 +3468,7 @@ export {
     isUnapprovedAction,
     isForwardedAction,
     isDynamicExternalWorkflowSubmitFailedAction,
+    isDynamicExternalWorkflowApproveFailedAction,
     isWhisperActionTargetedToOthers,
     isTagModificationAction,
     isIOUActionMatchingTransactionList,
